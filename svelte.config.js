56d669a8126a92ac797dc6d50dfe8fe84fe38411
Tony Grimes <acidtone@tonygrimes.com>
2022-12-28 18:00:45 -0700

    added images, changed indents

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,25 @@
 import adapter from '@sveltejs/adapter-auto';
+import { mdsvex } from 'mdsvex';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
+  extensions: ['.svelte', '.md'],
+
+  preprocess: [
+    mdsvex({
+      // The default mdsvex extension is .svx; this overrides that.
+      extensions: ['.md'],
+
+      // Adds IDs to headings, and anchor links to those IDs. Note: must stay in this order to work.
+      rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings]
+    })
+  ],
+
+  kit: {
+    adapter: adapter()
+  }
 };
 
 export default config;
