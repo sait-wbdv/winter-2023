de1dfb15589fe5914649ea836ed1afcfddaf0aab
Tony Grimes <acidtone@tonygrimes.com>
2023-02-17 08:21:53 -0700

    pushed a3 due date

--- src/lib/content/assessments/cpnt-262/due-dates.js
+++ src/lib/content/assessments/cpnt-262/due-dates.js
@@ -26,7 +26,7 @@ export const cpnt262DueDates = [
   // },
   {
     slug: 'assignment-3',
-    due: 'Tuesday, February 21, @ 11:59pm',
+    due: 'Thursday, February 23, @ 11:59pm',
     status: 'published'
   },
   // {
