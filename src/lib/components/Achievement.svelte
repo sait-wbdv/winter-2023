72d3b74236137efa8ce51acf520b477e4608c5c4
Tony Grimes <acidtone@tonygrimes.com>
2023-02-22 08:55:17 -0700

    added d8

--- src/lib/components/Achievement.svelte
+++ src/lib/components/Achievement.svelte
@@ -1,10 +1,13 @@
 <script>
   export let status;
+  export let dueDate = true;
 </script>
 
 <section id="achievement" class='achievement'>
   <h2>Today's Achievement</h2>
+  {#if dueDate}
   <p>Due today at 8pm in Brightspace.</p>
+  {/if}
 
   {#if status === 'announced' || status === 'published'}
   <slot />
