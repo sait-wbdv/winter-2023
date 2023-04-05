5ad1effbeb97861457bd4875e9f0650c6f4a59f0
Tony Grimes <acidtone@tonygrimes.com>
2023-04-05 08:49:45 -0600

    added 265 d1, d2, assessments

---
+++ src/lib/content/assessments/cpnt-265/due-dates.js
@@ -0,0 +1,17 @@
+export const cpnt265DueDates = [
+  {
+    slug: 'achievements-1',
+    due: 'Thursday, April 13th @ 11:59pm',
+    status: 'published'
+  },
+  {
+    slug: 'achievements-2',
+    due: 'Wednesday, April 19th @ 11:59pm',
+    status: 'published'
+  },
+  {
+    slug: 'final-project',
+    due: 'Friday, April 21st, @ 9am',
+    status: 'published'
+  }
+]
